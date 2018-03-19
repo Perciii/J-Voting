19fb0608b58db069bf0b7e884777a21adc9884b4
qsauvage <qs.sauvage@gmail.com>
2018-03-19 23:12:37 +0100

    add constructors, getters and setters of StrictProfile class

--- J-Voting/src/contract2/StrictProfile.java
+++ J-Voting/src/contract2/StrictProfile.java
@@ -1,7 +1,47 @@
 package contract2;
 
-public class StrictProfile {
-	private StrictPreference personnalPreferences;
-	private String name;
+import contract1.StrictPreference;
+import contract1.Alternative;
 
+/**
+ * 
+ * @author quentinsauvage & vincentnavales
+ *
+ */
+public class StrictProfile {
+	private StrictPreference personalPreferences;
+	private int voterId;
+	
+	/**
+	 * default constructor
+	 */
+	public StrictProfile(){
+		this.personalPreferences = new StrictPreference();
+		this.voterId = 0;
+	}
+	
+	/**
+	 * @param voterId the id of the voter
+	 * @param strictPreference the strict preferences of this voter
+	 */
+	public StrictProfile(int voterId, StrictPreference strictPreference){
+		this.personalPreferences = strictPreference;
+		this.voterId = voterId;
+	}
+	
+	/**
+	 * getters and setters
+	 */
+	public StrictPreference getPersonalPreferences() {
+		return personalPreferences;
+	}
+	public void setPersonalPreferences(StrictPreference personalPreferences) {
+		this.personalPreferences = personalPreferences;
+	}
+	public int getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(int voterId) {
+		this.voterId = voterId;
+	}
 }
