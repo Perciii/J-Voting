c6e03a83c01ee9fdb70cb538c858c2177a70e428
AdeleThornerMIAGE <thorneradele2008@yahoo.fr>
2018-04-21 11:14:13 +0200

    ReadProfile javadoc and logging  and corrections

--- src/main/java/io/github/oliviercailloux/y2018/j_voting/ReadProfile.java
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/ReadProfile.java
@@ -7,7 +7,8 @@ import org.slf4j.*;
 
 public class ReadProfile {
 	
-	static Logger log = LoggerFactory.getLogger(ReadProfile.class.getName());
+	static Logger log = LoggerFactory.getLogger(ReadProfile.class.getName());	
+	private static int id =1;// id is the id of the next voter that will be created in the profile
 	/**
 	 * @param path a string : the path of the file to read 
 	 * @return fileRead, a list of String where each element is a line of the SOC or SOI file read
@@ -15,6 +16,7 @@ public class ReadProfile {
 	 */
 	public static List<String> fromSOCorSOI(String path) throws IOException {
 		log.debug("fromSOCorSOI : \n") ;
+		Objects.requireNonNull(path);
 		log.debug("parameter : path = {}\n",path);
 		InputStream f= ReadProfile.class.getClassLoader().getResourceAsStream(path);
 		try(BufferedReader in = new BufferedReader(new InputStreamReader(f))){
@@ -35,6 +37,7 @@ public class ReadProfile {
 	 */
 	public static void displayProfileFromReadFile(List<String> fileRead){
 		log.debug("displayProfileFromReadFile : \n") ;
+		Objects.requireNonNull(fileRead);
 		log.debug("parameter : fileRead = {}\n",fileRead);
 		Iterator<String> it = fileRead.iterator();
 		while(it.hasNext()){
@@ -42,9 +45,16 @@ public class ReadProfile {
 		}
 	}
 	
-	
-	public static StrictPreference GetAlternatives(int nbAlternatives,List<String> file){
+	/**
+	 * 
+	 * @param nbAlternatives the number of alternatives in the profile
+	 * @param file a list of strings each containing an alternative
+	 * @return the alternatives in the profile given in a list of string.
+	 */
+	public static StrictPreference getAlternatives(int nbAlternatives,List<String> file){
 		log.debug("GetAlternatives :") ;
+		Objects.requireNonNull(nbAlternatives);
+		Objects.requireNonNull(file);
 		log.debug("parameters : nbAlternatives = {}, file = {}\n",nbAlternatives,file); 
 		Iterator<String> it = file.iterator();
 		String s1; 
@@ -63,9 +73,14 @@ public class ReadProfile {
 	}
 	
 	
-	
-	public static List<Integer> GetNbVoters(String s){
+	/**
+	 * 
+	 * @param s the line with the voters statistics (number, sum of count, number of unique alternatives)
+	 * @return a list with the three computed statistics
+	 */
+	public static List<Integer> getStatsVoters(String s){
 		log.debug("GetNbVoters :") ;
+		Objects.requireNonNull(s);
 		log.debug("parameter : s={}\n",s);
 		List<Integer> list=new ArrayList<Integer>();
 		String[] line=s.split(",");
@@ -76,66 +91,81 @@ public class ReadProfile {
 		return list;
 	}
 	
-	public static StrictPreference GetPreferences(StrictPreference listeAlternatives, String s1){
+	/**
+	 * 
+	 * @param listeAlternatives the alternatives of the profile
+	 * @param s1 a line of the profile containing the number of voters for a preference followed by the preference (list of alternatives)
+	 * @return the StrictPreference given in the line s1
+	 */
+	public static StrictPreference getPreferences(StrictPreference listeAlternatives, String s1){
+		log.debug("getPreferences\n");
+		Objects.requireNonNull(listeAlternatives);
+		Objects.requireNonNull(s1);
+		log.debug("parameters : listeAlternatives {} , s1 {}\n",listeAlternatives,s1);
 		String [] s2=s1.split(",");
 		LinkedHashSet<Alternative> pref= new LinkedHashSet<Alternative>();
-		size=Integer.parseInt(s2[0].trim());
-		for(int j=1;j<s2.length;j++){//we collect all the alternatives
-			Iterator<Alternative> i1=listeAlternatives.getPreferences().iterator();
-			Alternative a=i1.next();
-			boolean alternative_ok=false;
-			while(i1.hasNext()&&!alternative_ok){
-				if(a.getId()==(Integer.parseInt(s2[j].trim()))){
-					alternative_ok=true;
-				}
-				else{
-					a=i1.next();
-				}
+		for(String strAlter : s2){//we collect all the alternatives
+			Alternative alter = new Alternative(Integer.parseInt(strAlter.trim()));
+			log.debug("next alternative {}\n",alter);
+			if(listeAlternatives.contains(alter)) {
+				log.debug("correct alternative");
+				pref.add(alter);
 			}
-			pref.add(a);
+			else {
+				log.debug("alternative not in the profile");
+				throw new IllegalArgumentException("The line s1 contains an alternative that is not in the profile's alternatives");
+			}
 		}
 		return new StrictPreference(pref);
 	}
 	
-	
-	
-	private static int size =0;
-	private static int id =1;// id is the id for the voters we will create
-	
-	public static StrictProfile CreateProfiles(StrictPreference profil, int nbVoters){
-		StrictProfile SProfile = new StrictProfile();
-		for(int m=0;m<size;m++){//we create as many profiles as voters 
+	/**
+	 * 
+	 * @param pref a StrictPreference
+	 * @param nbVoters the number of voters that voted for the preference as parameter
+	 * @param profile the StrictProfile to which the votes will be added
+	 */
+	public static void addVotes(StrictPreference pref, int nbVoters, StrictProfile profile){
+		log.debug("addVotes\n");
+		Objects.requireNonNull(pref);
+		Objects.requireNonNull(nbVoters);
+		Objects.requireNonNull(profile);
+		log.debug("parameters : nbVoters {} for the preference {}\n",nbVoters,pref); 
+		for(int m=0 ; m<nbVoters ; m++){//we create as many profiles as voters 
 			Voter v =new Voter(id);
+			log.debug("adds th voter {} and the pref as parameter to the profile\n",id);
 			id++;
-			if (id-1>nbVoters){//we can't create more voters than the number of voters
-				throw new Error("Error: i>nbVoters");
-			}
-
-			System.out.println("Voter"+v.getId()+"pref"+profil);
-			SProfile.addProfile(v,profil);
+			profile.addProfile(v,pref);
 		}
-		return SProfile;
 	}
 
-	
-	public static StrictProfile GetProfiles(List<String> file, StrictPreference listeAlternatives, int nbVoters){
+	/**
+	 * 
+	 * @param file the lines with the number of votes for each preference
+	 * @param listAlternatives the alternatives of the profile
+	 * @param nbVoters the number of voters
+	 * @return the created StrictProfile
+	 */
+	public static StrictProfile buildProfile(List<String> file, StrictPreference listAlternatives, int nbVoters){
 		log.debug("GetProfiles :") ;
+		Objects.requireNonNull(file);
+		Objects.requireNonNull(listAlternatives);
+		Objects.requireNonNull(nbVoters);
 		Iterator<String> it = file.iterator();
-		StrictProfile SProfile = new StrictProfile();
+		StrictProfile profile = new StrictProfile();
 		String s1; //where we store the current line
 		while(it.hasNext()){
 			s1=it.next();
 			if (!s1.contains(",")){// if the line doesn't contain , it's the line of an alternative
-				throw new Error("Error: nbAlternative is not correct");
+				throw new IllegalArgumentException("the first string of file is an alternative line.");
 			}
 			else{
-				StrictPreference profil=GetPreferences(listeAlternatives,s1);
-				SProfile =CreateProfiles(profil, nbVoters);
+				StrictPreference pref=getPreferences(listAlternatives,s1);
+				addVotes(pref, nbVoters,profile);
 			}
 
 		}
-		return SProfile;
-
+		return profile;
 	}
 	
 
@@ -147,6 +177,7 @@ public class ReadProfile {
 
 	public static StrictProfile createProfileFromReadFile(List<String> fileRead){
 		log.debug("createProfileFromReadFile : \n") ;
+		Objects.requireNonNull(fileRead);
 		Iterator<String> it = fileRead.iterator();
 		StrictProfile SProfile= new StrictProfile();
 		String line_nb_voters;
@@ -160,9 +191,9 @@ public class ReadProfile {
 		while(it.hasNext()){//get the rest of the file
 			Profiles.add(it.next());
 		}
-		StrictPreference listeAlternatives =GetAlternatives(nbAlternatives,Alternatives);
-		List<Integer> list_int= GetNbVoters(line_nb_voters);
-		SProfile=GetProfiles(Profiles,listeAlternatives, list_int.get(0));
+		StrictPreference listeAlternatives =getAlternatives(nbAlternatives,Alternatives);
+		List<Integer> list_int= getStatsVoters(line_nb_voters);
+		SProfile=buildProfile(Profiles,listeAlternatives, list_int.get(0));
 		return SProfile;
 	}
 	
