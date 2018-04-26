febfd01fc2ed33c64a440d083799f4626b0155cd
LaetitiaCourgey <laetitiacourgey@gmail.com>
2018-04-27 00:47:25 +0200

    Bordas class

---
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/AlternativeScore.java
@@ -0,0 +1,35 @@
+package io.github.oliviercailloux.y2018.j_voting;
+
+import java.util.Objects;
+
+public class AlternativeScore {
+	
+	private int score;
+	private Alternative alt;
+	
+	public AlternativeScore(Alternative a, int s){
+		log.debug("AlternativeScore\n");
+		Objects.requireNonNull(s);
+		log.debug("parameter Alternative : {}\n", a.toString());
+		log.debug("parameter Score : {}\n", Integer.toString(score));
+		alt=a;
+		score=s;
+	}
+	
+	public Alternative getA(){
+		log.debug("getA\n");
+		return alt;
+	}
+	
+	public int getS(){
+		log.debug("getS\n");
+		return score;
+	}
+	
+	
+	public String toString(){
+		return ("A : " + alt.toString() +" S : "+ Integer.toString(score) +'\n');
+		
+	}
+	
+}
