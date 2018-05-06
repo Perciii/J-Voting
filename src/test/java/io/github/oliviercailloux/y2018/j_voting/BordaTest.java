6817bd6f953e0d21d21000c5d1efeb88e54f6595
LaetitiaCourgey <laetitiacourgey@gmail.com>
2018-05-06 14:25:31 +0200

    correcting some errors in testBorda

--- src/test/java/io/github/oliviercailloux/y2018/j_voting/BordaTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/j_voting/BordaTest.java
@@ -9,22 +9,34 @@ public class BordaTest {
 	
 	@Test
 	public void testgetSocietyStrictPreference(){
-		StrictProfile sProfile = new StrictProfile();
+		StrictProfile profile = new StrictProfile();
 		Alternative a1 = new Alternative(1);
 		Alternative a2 = new Alternative(2);
 		Alternative a3 = new Alternative(3);
 		Voter v1 = new Voter(1);
 		Voter v2 = new Voter(2);
-		List<Alternative> alternatives = new ArrayList<Alternative>();
-		alternatives.add(a1);
-		alternatives.add(a2);
-		StrictPreference pref1 = new StrictPreference(alternatives);
-		alternatives.add(a3);
-		StrictPreference pref2 = new StrictPreference(alternatives);
-		sProfile.addProfile(v1, pref1);
-		sProfile.addProfile(v2, pref2);
-		System.out.println("profile : "+sProfile);	
-		StrictPreference sPref = new Borda().getSocietyStrictPreference(sProfile);
+		Voter v3 = new Voter(3);
+		Voter v4 = new Voter(4);
+		Voter v5 = new Voter(5);
+		Voter v6 = new Voter(6);
+		ArrayList<Alternative> list1 = new ArrayList<Alternative>();
+		ArrayList<Alternative> list2 = new ArrayList<Alternative>();
+		list1.add(a1);
+		list1.add(a2);
+		list1.add(a3);
+		list2.add(a3);
+		list2.add(a2);
+		list2.add(a1);
+		StrictPreference pref1 = new StrictPreference(list1);
+		StrictPreference pref2 = new StrictPreference(list2);
+		profile.addProfile(v1, pref1);
+		profile.addProfile(v2, pref1);
+		profile.addProfile(v3, pref1);
+		profile.addProfile(v4, pref1);
+		profile.addProfile(v5, pref2);
+		profile.addProfile(v6, pref2);
+		System.out.println("profile : "+profile);	
+		StrictPreference sPref = new Borda().getSocietyStrictPreference(profile);
 		System.out.println("preference : "+sPref);	
 		
 	}
@@ -46,22 +58,34 @@ public class BordaTest {
 	
 	@Test
 	public void testgetScoresProfile() {
-		StrictProfile p = new StrictProfile();
+		StrictProfile profile = new StrictProfile();
 		Alternative a1 = new Alternative(1);
 		Alternative a2 = new Alternative(2);
 		Alternative a3 = new Alternative(3);
 		Voter v1 = new Voter(1);
 		Voter v2 = new Voter(2);
-		List<Alternative> alternatives = new ArrayList<Alternative>();
-		alternatives.add(a1);
-		alternatives.add(a2);
-		StrictPreference pref1 = new StrictPreference(alternatives);
-		alternatives.add(a3);
-		StrictPreference pref2 = new StrictPreference(alternatives);
-		p.addProfile(v1, pref1);
-		p.addProfile(v2, pref2);
-		List<AlternativeScore> scores = Borda.getScore(p);
-		System.out.println("profile : "+p+'\n'+'\n'+"Scores : " + scores);	
+		Voter v3 = new Voter(3);
+		Voter v4 = new Voter(4);
+		Voter v5 = new Voter(5);
+		Voter v6 = new Voter(6);
+		ArrayList<Alternative> list1 = new ArrayList<Alternative>();
+		ArrayList<Alternative> list2 = new ArrayList<Alternative>();
+		list1.add(a1);
+		list1.add(a2);
+		list1.add(a3);
+		list2.add(a3);
+		list2.add(a2);
+		list2.add(a1);
+		StrictPreference pref1 = new StrictPreference(list1);
+		StrictPreference pref2 = new StrictPreference(list2);
+		profile.addProfile(v1, pref1);
+		profile.addProfile(v2, pref1);
+		profile.addProfile(v3, pref1);
+		profile.addProfile(v4, pref1);
+		profile.addProfile(v5, pref2);
+		profile.addProfile(v6, pref2);
+		List<AlternativeScore> scores = Borda.getScore(profile);
+		System.out.println("profile : "+profile+'\n'+'\n'+"Scores : " + scores);	
 	}
 	
 	
@@ -97,22 +121,34 @@ public class BordaTest {
 	
 	@Test
 	public void testgetSortedScores(){
-		StrictProfile p = new StrictProfile();
+		StrictProfile profile = new StrictProfile();
 		Alternative a1 = new Alternative(1);
 		Alternative a2 = new Alternative(2);
 		Alternative a3 = new Alternative(3);
 		Voter v1 = new Voter(1);
 		Voter v2 = new Voter(2);
-		List<Alternative> alternatives = new ArrayList<Alternative>();
-		alternatives.add(a1);
-		alternatives.add(a2);
-		StrictPreference pref1 = new StrictPreference(alternatives);
-		alternatives.add(a3);
-		StrictPreference pref2 = new StrictPreference(alternatives);
-		p.addProfile(v1, pref1);
-		p.addProfile(v2, pref2);
-		System.out.println("before : "+p);	
-		List<AlternativeScore> listScoresSorted = Borda.getSortedScores(p);
+		Voter v3 = new Voter(3);
+		Voter v4 = new Voter(4);
+		Voter v5 = new Voter(5);
+		Voter v6 = new Voter(6);
+		ArrayList<Alternative> list1 = new ArrayList<Alternative>();
+		ArrayList<Alternative> list2 = new ArrayList<Alternative>();
+		list1.add(a1);
+		list1.add(a2);
+		list1.add(a3);
+		list2.add(a3);
+		list2.add(a2);
+		list2.add(a1);
+		StrictPreference pref1 = new StrictPreference(list1);
+		StrictPreference pref2 = new StrictPreference(list2);
+		profile.addProfile(v1, pref1);
+		profile.addProfile(v2, pref1);
+		profile.addProfile(v3, pref1);
+		profile.addProfile(v4, pref1);
+		profile.addProfile(v5, pref2);
+		profile.addProfile(v6, pref2);
+		System.out.println("before : "+profile);	
+		List<AlternativeScore> listScoresSorted = Borda.getSortedScores(profile);
 		System.out.println("after : "+listScoresSorted);	
 	}
 	
