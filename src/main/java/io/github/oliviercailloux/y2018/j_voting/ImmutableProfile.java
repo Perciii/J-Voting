3f6d682536056e75d3f060015c6dc31f5342ea3c
AdeleThornerMIAGE <thorneradele2008@yahoo.fr>
2018-05-23 21:15:59 +0200

    create ImmutableProfiles and did ImmutableProfileI

---
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/ImmutableProfile.java
@@ -0,0 +1,5 @@
+package io.github.oliviercailloux.y2018.j_voting;
+
+public class ImmutableProfile implements Profile{
+
+}
---
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/ImmutableStrictProfileI.java
@@ -0,0 +1,5 @@
+package io.github.oliviercailloux.y2018.j_voting;
+
+public class ImmutableStrictProfileI implements StrictProfileI{
+
+}
