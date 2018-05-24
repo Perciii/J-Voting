0b07880756c6374ae6064cef05a8893d97ec7790
LaetitiaCourgey <laetitiacourgey@gmail.com>
2018-05-24 23:18:13 +0200

    Dictator

---
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/Dictator.java
@@ -0,0 +1,41 @@
+package io.github.oliviercailloux.y2018.j_voting;
+
+import org.slf4j.*;
+import com.google.common.base.Preconditions;
+import java.util.*;
+
+
+
+public class Dictator implements SocialWelfareFunction{
+	
+	private Voter Dictator;
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(Borda.class.getName());	
+	
+	public Dictator (Voter v){
+		Preconditions.checkNotNull(v);
+		Dictator = v;
+	}
+	
+	/***
+	 * 
+	 * @param profile
+	 * @return the dictator's preference
+	 */
+	@Override
+	public Preference getSocietyPreference(ProfileI profile) {
+		LOGGER.debug("getSocietyStrictPreference");
+		
+		Preconditions.checkNotNull(profile);
+		Preconditions.checkArgument(profile.contains(Dictator));
+		
+		LOGGER.debug("parameter profile : {}", profile);
+		LOGGER.debug("Dictator : {}", Dictator);
+		LOGGER.debug("return preference : {}", profile.getPreference(Dictator));
+		
+		return profile.getPreference(Dictator);
+
+	}
+
+
+}
