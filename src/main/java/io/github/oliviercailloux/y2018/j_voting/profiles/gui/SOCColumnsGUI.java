43ace2531439870b21a31e4c1a63f927e2d2701a
Perciii <qs.sauvage@gmail.com>
2018-06-22 13:53:14 +0200

    make createColumns a void and beginning of impl of add Alternative

--- src/main/java/io/github/oliviercailloux/y2018/j_voting/profiles/gui/SOCColumnsGUI.java
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/profiles/gui/SOCColumnsGUI.java
@@ -1,26 +1,103 @@
 package io.github.oliviercailloux.y2018.j_voting.profiles.gui;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import org.eclipse.swt.*;
-import org.eclipse.swt.events.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
-import org.slf4j.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 
-import io.github.oliviercailloux.y2018.j_voting.*;
-import io.github.oliviercailloux.y2018.j_voting.profiles.*;
-import io.github.oliviercailloux.y2018.j_voting.profiles.management.*;
+import io.github.oliviercailloux.y2018.j_voting.Alternative;
+import io.github.oliviercailloux.y2018.j_voting.Voter;
+import io.github.oliviercailloux.y2018.j_voting.profiles.StrictProfile;
 
 public class SOCColumnsGUI extends ColumnsDefaultGUI {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SOCColumnsGUI.class.getName());
+	private int alternativeToAdd;
 
 	@Override
-	public List<String> createColumns() {
+	public void tableDisplay(String fileName) {
+		LOGGER.debug("tableDisplay");
+
+		// table layout handling
+		mainShell.setLayout(new GridLayout());
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+		table.setLayoutData(data);
+
+		Label label = new Label(mainShell, SWT.NULL);
+		label.setText("New alternative : ");
+
+		final Text newAlternativeText = new Text(mainShell, SWT.SINGLE | SWT.BORDER);
+		final Button addAlternativeButton = new Button(mainShell, SWT.PUSH);
+
+		newAlternativeText.addListener(SWT.Modify, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				try {
+					alternativeToAdd = Integer.parseInt(newAlternativeText.getText());
+					addAlternativeButton.setEnabled(true);
+				} catch (IllegalArgumentException iae) {
+					LOGGER.debug("Illegal Argument Exception : " + iae);
+					addAlternativeButton.setEnabled(false);
+				}
+			}
+		});
+
+		addAlternativeButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				addAlternative(alternativeToAdd);
+			}
+		});
+
+		createColumns();
+		populateRows();
+		handleDragAndDrop();
+		checkRadioButton();
+
+		TableColumn[] tableColumns = table.getColumns();
+		for (TableColumn tableColumn : tableColumns) {
+			tableColumn.pack();
+		}
+
+		saveButton.setText("Save");
+		saveButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				save(fileName);
+			}
+		});
+	}
+
+	public void addAlternative(int alternative) {
+		LOGGER.debug("addAlternative :");
+		Preconditions.checkNotNull(alternative);
+
+		StrictProfile strictProfile = profileBuilder.createStrictProfile();
+		TableItem item = new TableItem(table, SWT.NONE);
+		List<Integer> altForEveryone = Collections.nCopies(strictProfile.getNbAlternatives(), alternative);
+		item.setText(altForEveryone.toArray(new Integer[strictProfile.getNbAlternatives()]).toString());
+	}
+
+	@Override
+	public void createColumns() {
 		LOGGER.debug("createColumns :");
 		// if profile get from file is SOC, create a StrictProfile from it
 		StrictProfile strictProfile = profileBuilder.createStrictProfile();
@@ -36,20 +113,16 @@ public class SOCColumnsGUI extends ColumnsDefaultGUI {
 			TableColumn column = new TableColumn(table, SWT.NONE);
 			column.setText(title);
 		}
-
-		return titles;
 	}
 
 	@Override
 	public void populateRows() {
 		LOGGER.debug("populateRows :");
-		// ROWS
 		StrictProfile strictProfile = profileBuilder.createStrictProfile();
+		// ROWS
 		List<String> alternatives = new ArrayList<>();
 
-		int nbAlternatives = strictProfile.getNbAlternatives();
-
-		for (int i = 0; i < nbAlternatives; i++) {
+		for (int i = 0; i < strictProfile.getNbAlternatives(); i++) {
 			// get ith alternative of each voter
 			List<Alternative> ithAlternatives = strictProfile.getIthAlternatives(i);
 			for (Alternative alt : ithAlternatives) {
@@ -57,7 +130,8 @@ public class SOCColumnsGUI extends ColumnsDefaultGUI {
 			}
 
 			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText(alternatives.toArray(new String[nbAlternatives])); // create a row with ith alternatives
+			item.setText(alternatives.toArray(new String[strictProfile.getNbAlternatives()])); // create a row with ith
+																								// alternatives
 			alternatives.clear(); // empty the list
 		}
 	}
